d27ef8ce271e08c6d8bea6df307cc37a55c1f4fe
Gabe Biolos <gabe@farylrobin.com>
2025-04-22 10:53:53 -0400

    Build project and update docs for GitHub Pages deployment

--- TheNestSrc/AnalyticsHub/Amazon/FileUploads/vite.config.js
+++ TheNestSrc/AnalyticsHub/Amazon/FileUploads/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/TheNestSrc/AnalyticsHub/Amazon/FileUploads/',
   plugins: [react()],
+  build: {
+    outDir: '/Users/gabebiolos/Library/CloudStorage/Box-Box/FR_BOX/FR_General/z_Infrastructure/farylrobin.github.io/docs'
+  },
 });
